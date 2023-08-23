68546b05bf67c5611e83b9f0e6a86a9fdbd7d432
Italo souza sampaio <italo12346@gmail.com>
2023-08-23 15:38:10 -0300

    Criando conexão com banco de dados

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
+const connection = require('./database/connection')
+const bodyParser = require('body-parser')
+
 const port = 3000
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!");
+    }).catch((error) => {
+        console.log(error);
+    })
 
 app.get("/", (req,res)=>{
     res.send("ok")

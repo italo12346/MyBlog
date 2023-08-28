63553b7f04f6c9aff1462d91a65110fc14764576
Italo souza sampaio <italo12346@gmail.com>
2023-08-28 15:50:42 -0300

    Iniciando controller de artigos

--- controller/articlesController.js
+++ controller/articlesController.js
@@ -1,8 +1,18 @@
-const express = require('express')
+const express = require("express")
+const Article = require("../model/Article")
 const router = express.Router() 
 
 router.get('/admin/article',(req, res)=>{
-    res.render('admin/articles/article')
+    Article.findAll().then(articles=>{
+        res.render('admin/articles/article',{
+            article:articles
+        })
+    })
+
+})
+
+router.get('/admin/article/new',(req, res)=>{
+    
 })
 
 module.exports = router

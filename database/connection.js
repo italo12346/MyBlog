7a90d0ac8f027cbaaf345193c8dc08b61820c339
Italo souza sampaio <italo12346@gmail.com>
2023-09-23 10:32:32 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
-const  Sequelize = require("sequelize")
+const Sequelize = require("sequelize")
 
 const connection = new Sequelize('myBlog','root','123456',{
     host:'localhost',
     dialect:'mysql',
     timezone:"-03:00",
-    
+    logging: console.log, // Adicionando logging para visualizar mensagens de erro
 })
 
 module.exports = connection;

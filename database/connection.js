bc7ba711771d4860099f8840595be19003e908e2
Italo souza sampaio <italo12346@gmail.com>
2023-09-26 14:32:28 -0300

    fix: mudando tipo de banco de dados,
    A plataforma que estou fazendo o deploy não aceitou myslq

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,6 @@ require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    timezone: process.env.DB_TIMEZONE,
     logging: console.log, // Adicionando logging para visualizar mensagens de erro
 });
 
